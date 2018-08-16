9e710bb612d05b3ebb691b54777439b92dc75296
shmilyou <530060499@qq.com>
2018-08-16 11:42:10 +0800

    test controller and .jpg

---
+++ src/main/java/com/shmilyou/controller/vo/IndexController.java
@@ -0,0 +1,18 @@
+package com.shmilyou.controller.vo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with 岂止是一丝涟漪
+ * Date: 2018/8/16
+ */
+@Controller
+@RequestMapping("/phisoa")
+public class IndexController {
+
+    @RequestMapping(value = {"/index","/",""})
+    public String index(){
+        return "index";
+    }
+}
