cb15331a0cf2dd3b41f0717ec3cd7bb9bec1660b
shmilyou <530060499@qq.com>
2018-08-16 12:07:54 +0800

    use lombock

--- src/main/java/com/shmilyou/entity/Category.java
+++ src/main/java/com/shmilyou/entity/Category.java
@@ -1,9 +1,12 @@
 package com.shmilyou.entity;
 
+import lombok.Data;
+
 /**
  * Created with 岂止是一丝涟漪
  * Date: 2018/8/14
  */
+@Data
 public class Category extends BaseEntity {
 
     private String id;
--- src/main/java/com/shmilyou/entity/OpenCourse.java
+++ src/main/java/com/shmilyou/entity/OpenCourse.java
@@ -1,9 +1,12 @@
 package com.shmilyou.entity;
 
+import lombok.Data;
+
 /**
  * Created with 岂止是一丝涟漪
  * Date: 2018/8/14
  */
+@Data
 public class OpenCourse extends BaseEntity {
 
     private String id;
