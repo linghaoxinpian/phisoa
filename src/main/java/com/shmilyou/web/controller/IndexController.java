118e0d61f1d192d82df79e42d8138bb627f82b7c
shmilyou <530060499@qq.com>
2018-08-19 16:45:54 +0800

    add todo

--- src/main/java/com/shmilyou/web/controller/IndexController.java
+++ src/main/java/com/shmilyou/web/controller/IndexController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created with 岂止是一丝涟漪
@@ -72,13 +73,49 @@ public class IndexController extends BaseController {
      * @param classify  0:机构，1：爱好者，2：标签
      */
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    @ResponseBody
-    public Object search(String searchStr, String classify, Integer pageIndex, Integer pageSize) {
+    public String search(String searchStr, String classify, Integer pageIndex, Integer pageSize) {
         pageIndex = 1;
         pageSize = 20;
         //todo：根据【0.机构名/1.爱好者名/2.标签】 搜索
 
+        return "searchResult";
+    }
+
+    @RequestMapping(value = "/news", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Object> news() {
+        //待定：行业资讯：展示文化动态机构前沿，多文化培训的重要性以及优秀机构展示
         return null;
     }
 
+    @RequestMapping(value = "/aside", method = RequestMethod.POST)
+    @ResponseBody
+    public List<Object> aside() {
+        //待定：机构栏目【参考腾讯网、】：该地区所有机构情况【若首推机构或个人饱和，此处依次往后展示】
+        return null;
+    }
+
+    @RequestMapping(value = "/place_amateur_list", method = RequestMethod.POST)
+    @ResponseBody
+    public List<Object> amateurNews() {
+        //待定：个人栏目：该地区所有个人情况
+        return null;
+    }
+
+    @RequestMapping(value = " to_denounce", method = RequestMethod.GET)
+    public String toDenounce() {
+        return "to_denounce";
+    }
+
+    /**
+     * 举报提交
+     *
+     * @param profile     举报人姓名
+     * @param isAnonymity 是否匿名
+     * @param content     举报内容
+     */
+    @RequestMapping(value = " to_denounce", method = RequestMethod.POST)
+    public String toDenounce(String profile, String email, String phone, String isAnonymity, String content) {
+        return "to_denounce";
+    }
 }
