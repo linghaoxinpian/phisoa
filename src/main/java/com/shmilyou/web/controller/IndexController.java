ea0ed88e16644561e2a7dcfde78e4f340eae8f53
shmilyou <530060499@qq.com>
2018-10-26 09:36:45 +0800

    update

--- src/main/java/com/shmilyou/web/controller/IndexController.java
+++ src/main/java/com/shmilyou/web/controller/IndexController.java
@@ -5,23 +5,18 @@ import com.shmilyou.entity.Category;
 import com.shmilyou.entity.Course;
 import com.shmilyou.entity.OpenCourse;
 import com.shmilyou.entity.Organization;
-import com.shmilyou.entity.User;
 import com.shmilyou.service.AmateurService;
 import com.shmilyou.service.CategoryService;
 import com.shmilyou.service.CourseService;
 import com.shmilyou.service.OpenCourseService;
 import com.shmilyou.service.OrganizationService;
 import com.shmilyou.service.UserService;
-import com.shmilyou.utils.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -59,58 +54,6 @@ public class IndexController extends BaseController {
         //6.排行【机构】【爱好者】
         List<Amateur> top1 = amateurService.indexRecommendTop();
         List<Organization> top2 = organizationService.indexRecommendTop();
-        return "index";
-    }
-
-    @RequestMapping(value = "/login_in_user", method = RequestMethod.GET)
-    public String loginIn() {
-        return "login_user";
-    }
-
-    @RequestMapping(value = "/login_in_user", method = RequestMethod.POST)
-    public String loginInUser(String account, String password, HttpServletRequest request, ModelMap modelMap) {
-        //求学者登录校验
-        if (!StringUtils.isEmpty(account) && !StringUtils.isEmpty(password)) {
-            User user = userService.loginIn(account, password);
-            if (user != null) {
-                request.getSession().setAttribute(Constant.LOGIN_USER, user);
-                return "index";
-            }
-        }
-        modelMap.addAttribute("error", "用户名或密码错误");
-        modelMap.addAttribute("account", account);
-        return "login_user";
-    }
-
-    @RequestMapping(value = "/login_in_organization", method = RequestMethod.POST)
-    public String loginInOrganization(String account, String password, HttpServletRequest request) {
-        //机构登录校验
-        if (StringUtils.isEmpty(account) && StringUtils.isEmpty(password)) {
-            Organization organization = organizationService.loginIn(account, password);
-            if (organization != null) {
-                request.getSession().setAttribute(Constant.LOGIN_ORGANIZATION, organization);
-                return "index";
-            }
-        }
-        return "login";
-    }
-
-    @RequestMapping(value = "/login_in_amateur", method = RequestMethod.POST)
-    public String loginInAmateur(String account, String password, HttpServletRequest request) {
-        //爱好者登录校验
-        if (StringUtils.isEmpty(account) && StringUtils.isEmpty(password)) {
-            Amateur amateur = amateurService.loginIn(account, password);
-            if (amateur != null) {
-                request.getSession().setAttribute(Constant.LOGIN_AMATEUR, amateur);
-                return "index";
-            }
-        }
-        return "login";
-    }
-
-    @RequestMapping(value = "/login_out", method = RequestMethod.GET)
-    public String loginOut(HttpServletRequest request) {
-        request.getSession().invalidate();
         return "index";
     }
 
